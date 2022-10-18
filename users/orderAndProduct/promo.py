5d70b4c2806396b5a818ab7f50762fdff63d5254
IlyaDischenko <ilyadischenko@gmail.com>
2022-10-18 22:06:19 +0300

    добавил минимальную сумму

--- users/orderAndProduct/promo.py
+++ users/orderAndProduct/promo.py
@@ -4,104 +4,39 @@ from sqlalchemy import create_engine, select, Table, Column, Integer, String, Me
 db_connect = config('database-connect-addres')
 meta = MetaData()
 
-discount = Table('discount', meta,
-                         Column('id', Integer(), primary_key=True),
-                         Column('promocode', String(255), unique=True),
-                         Column('count', Integer()),
-                         Column('type', String(255)),
+discount = Table('discount1', meta,
+                 Column('id', Integer(), primary_key=True),
+                 Column('promocode', String(255), unique=True),
+                 Column('count', Integer()),
+                 Column('type', String(255)),
 
-                         Column('return_data', String()),
+                 Column('return_data', String()),
 
-                         Column('need_number', Boolean()),
-                         Column('number', String(255)),
-                         Column('is_view', Boolean(), default=True),
-                         )
-
-# discount_rub = Table('discount_rub', meta,
-#                      Column('id', Integer(), primary_key=True),
-#                      Column('promocode', String(255), unique=True),
-#                      Column('count', Integer()),
-#                      Column('rub', Integer()),
-#                      Column('type', Boolean()),
-#                      Column('number', String(255)),
-#                      Column('is_view', Boolean(), default=True),
-#                      )
-#
-# promo_items = Table('promo_items', meta,
-#                     Column('id', Integer(), primary_key=True),
-#                     Column('promocode', String(255), unique=True),
-#                     Column('title', String(255)),
-#                     Column('description', String(), default="None"),
-#                     Column('photo', String()),
-#                     Column('price', Integer()),
-#                     Column('count', Integer()),
-#                     Column('type', Boolean()),
-#                     Column('number', String(255)),
-#                     Column('is_view', Boolean(), default=True),
-#                     )
+                 Column('min_sum', Integer()),
+                 Column('need_number', Boolean()),
+                 Column('number', String(255)),
+                 Column('is_view', Boolean(), default=True),
+                 )
 
 
 def check_discount(promo, number):
-    sel = select([discount.c.id, discount.c.count, discount.c.type, discount.c.return_data,
+    sel = select([discount.c.id, discount.c.count, discount.c.type, discount.c.return_data, discount.c.min_sum,
                   discount.c.need_number, discount.c.number, discount.c.is_view]).where(discount.c.promocode == promo)
     res = conn.execute(sel).fetchall()
 
     if len(res) < 1:
         return {"status": 400}
     elif len(res) != 0:
-        if res[0][6] and res[0][1] != 0:
-            if not res[0][4]:
-                return {"type": res[0][2], "discount_data": res[0][3], "status": 200}
-            elif res[0][4] and res[0][5] != number:
+        if res[0][7] and res[0][1] != 0:
+            if not res[0][5]:
+                return {"type": res[0][2], "min_sum": res[0][4], "discount_data": res[0][3], "status": 200}
+            elif res[0][5] and res[0][6] != number:
                 return {"status": 401}
-            elif res[0][4] and res[0][5] == number:
-                return {"type": res[0][2], "discount_data": res[0][3], "status": 200}
-            # -1 К ИСПОЛЬЗОВАНИЮ СДЕЛАТЬ НЕ ЗАБЫТЬ НА ФУНКЦИЮ ИСПОЛЬЗОВАНИЯ ПРОМОКОДА
-            # ins = discount_percent.update().values(count = res[0][1] - 1).where(discount_percent.c.promocode == promo)
-            # fin = conn.execute(ins)
+            elif res[0][5] and res[0][6] == number:
+                return {"type": res[0][2], "min_sum": res[0][4], "discount_data": res[0][3], "status": 200}
 
         else:
             return {"status": 422}
-
-
-# def check_rub(promo):
-#     sel = select([discount_rub.c.rub, discount_rub.c.count, discount_rub.c.is_view]).where(
-#         discount_rub.c.promocode == promo)
-#     res = conn.execute(sel).fetchall()
-#
-#     if len(res) < 1:
-#         return False
-#     elif len(res) != 0:
-#         if res[0][2] == True and res[0][1] != 0:
-#             # -1 К ИСПОЛЬЗОВАНИЮ СДЕЛАТЬ НЕ ЗАБЫТЬ НА ФУНКЦИЮ ИСПОЛЬЗОВАНИЯ ПРОМОКОДА
-#             # ins = discount_percent.update().values(count = res[0][1] - 1).where(discount_percent.c.promocode == promo)
-#             # fin = conn.execute(ins)
-#             return res[0][0]
-#         else:
-#             return False
-
-
-# def check_items(promo):
-#     sel = select([promo_items.c.title, promo_items.c.description, promo_items.c.photo, promo_items.c.price,
-#                   promo_items.c.count, promo_items.c.is_view]).where(promo_items.c.promocode == promo)
-#     res = conn.execute(sel).fetchall()
-#
-#     if len(res) < 1:
-#         return False
-#     elif len(res) != 0:
-#         if res[0][5] == True and res[0][4] != 0:
-#             # -1 К ИСПОЛЬЗОВАНИЮ СДЕЛАТЬ НЕ ЗАБЫТЬ НА ФУНКЦИЮ ИСПОЛЬЗОВАНИЯ ПРОМОКОДА
-#             # ins = discount_percent.update().values(count = res[0][1] - 1).where(discount_percent.c.promocode == promo)
-#             # fin = conn.execute(ins)
-#             return {
-#                 "title": res[0][0],
-#                 "description": res[0][1],
-#                 "photo": res[0][2],
-#                 "price": res[0][3],
-#             }
-#
-#         else:
-#             return False
 
 
 engine = create_engine(db_connect, echo=False, pool_size=6)
